225c2c83e7aa7aba4821c0315b1fa0d51fe471a2
an154h <anisahisk@hotmail.com>
2026-08-13 15:09:08 +0800

    change testimonial video under testimonial section

--- src/components/landing/TestimonialsSection.tsx
+++ src/components/landing/TestimonialsSection.tsx
@@ -109,18 +109,18 @@ export function TestimonialsSection() {
           </CarouselContent>
         </Carousel>
       </div>
-      <div className="section-container">
-        <div className="relative w-full h-screen overflow-hidden mb-8">
-          <iframe
-            video src={Testimonial}
-            title="Testimonial Video"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen;"
-            allowfullscreen
-            className="enterprise-video items-center justify-center w-full h-full object-cover"
-          ></iframe>
-        </div>
-      </div>
     </section >
   );
 }
+<div className="section-container">
+  <div className="relative w-full h-screen overflow-hidden mb-8">
+    <iframe
+      video src={Testimonial}
+      title="Testimonial Video"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen;"
+      allowfullscreen
+      className="enterprise-video items-center justify-center w-full h-full object-cover"
+    ></iframe>
+  </div>
+</div>
